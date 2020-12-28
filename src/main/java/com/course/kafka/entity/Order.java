a07d6f410317dc3555fd43a2ace66b61d7f11825
sunny <sunny.keila24@gmail.com>
2020-12-28 20:21:01 +0100

    Initial commit

---
+++ src/main/java/com/course/kafka/entity/Order.java
@@ -0,0 +1,91 @@
+package com.course.kafka.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name ="orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private int orderId;
+
+    @Column(nullable = false,length = 20)
+    private String orderNumber;
+
+    @Column(nullable = false,length = 200)
+    private String orderLocation;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDateTime;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> items;
+
+    @Column(nullable = false,length = 20)
+    private String creditCardNumber;
+
+
+
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getOrderLocation() {
+        return orderLocation;
+    }
+
+    public void setOrderLocation(String orderLocation) {
+        this.orderLocation = orderLocation;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public void setOrderDateTime(LocalDateTime orderDateTime) {
+        this.orderDateTime = orderDateTime;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderNumber='" + orderNumber + '\'' +
+                ", orderLocation='" + orderLocation + '\'' +
+                ", orderDateTime=" + orderDateTime +
+                ", items=" + items +
+                '}';
+    }
+}
---
+++ src/test/java/com/course/kafka/KafkaOrderApplicationTests.java
@@ -0,0 +1,10 @@
+package com.course.kafka;
+
+
+class KafkaOrderApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
