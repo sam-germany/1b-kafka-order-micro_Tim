c7f2b441c83626c8c2f5f215d4017a75702b9546
sunny <sunny.keila24@gmail.com>
2021-01-06 10:57:35 +0100

    Vote updated

---
+++ src/main/java/com/course/kafka/broker/producer/WebColorVoteProducer.java
@@ -0,0 +1,18 @@
+package com.course.kafka.broker.producer;
+
+import com.course.kafka.broker.message.WebColorVoteMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebColorVoteProducer {
+
+	@Autowired
+	private KafkaTemplate<String, WebColorVoteMessage> kafkaTemplate;
+
+	public void publish(WebColorVoteMessage message) {
+		kafkaTemplate.send("t.commodity.web.vote-color", message.getUsername(), message);
+	}
+
+}
